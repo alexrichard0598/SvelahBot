bfa15092b039de6518af949db2a0b97c4a3d976f
svelah-kaldra <alex.richard0598@pm.me>
2022-02-14 15:39:47 -0400

    Updated clear command

--- src/model/mediaQueue.ts
+++ src/model/mediaQueue.ts
@@ -18,7 +18,7 @@ export class MediaQueue {
     hash.update(`${this.queue.length}${url}${Date.now()}`);
     const id = hash.digest("hex");
     video.id = id;
-    video.meta = meta? meta : await SharedMethods.getMetadata(url, enqueuedBy, null);
+    video.meta = meta ? meta : await SharedMethods.getMetadata(url, enqueuedBy, null);
     if (video.meta.title !== "") {
       this.queue.push(video);
     }
@@ -37,9 +37,13 @@ export class MediaQueue {
     }
   }
 
-  clear() {
-    this.queue.length = 0;
-    this.looping = false;
+  clear(keepCurrentSong = false) {
+    if (keepCurrentSong) {
+      this.queue.splice(1, this.queue.length);
+    } else {
+      this.queue.splice(0, this.queue.length);
+      this.looping = false;
+    }
   }
 
   getQueue(): Array<YouTubeVideo> {
@@ -59,7 +63,7 @@ export class MediaQueue {
   }
 
   async currentItem(): Promise<YouTubeVideo> {
-    if(this.queue[0] == undefined) return undefined;
+    if (this.queue[0] == undefined) return undefined;
     if (this.queue[0].resource == undefined || this.queue[0].resource.ended) {
       const typeUrl = await SharedMethods.determineMediaType(this.queue[0].url);
       if (typeUrl[0] == MediaType.yt_video || typeUrl[0] == MediaType.yt_search) {
@@ -88,14 +92,14 @@ export class MediaQueue {
     while (copyQueue.length > 0) {
       const j = Math.floor(Math.random() * (copyQueue.length))
       shuffledQueue.push(copyQueue[j]);
-      copyQueue = copyQueue.slice(0, j).concat(copyQueue.slice(j+1));
+      copyQueue = copyQueue.slice(0, j).concat(copyQueue.slice(j + 1));
     }
 
     this.queue = shuffledQueue;
   }
 
   removeItemAt(i: number) {
-    const newQueue = this.queue.slice(0, i).concat(this.queue.slice(i+1));
+    const newQueue = this.queue.slice(0, i).concat(this.queue.slice(i + 1));
     this.queue = newQueue;
   }
 }
